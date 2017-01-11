6a4202880ad2003cb9ee09322405a66a5faa7e36
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2017-01-10 22:55:48 -0500

    Extended the class WebMvcConfigurerAdapterwhich allows to override methods. Overrided addResourceHandlers to specify the location of pdf files under WEB-INF/pdf and added a mapping in our dispatcherServlet(WebAppInitializer). Added a pdf and validated that the statuc resource is available under http://localhost:8080/event-tracker/pdfs/creative-commons.pdf

--- src/main/java/com/coding/thoughtworks/WebAppInitializer.java
+++ src/main/java/com/coding/thoughtworks/WebAppInitializer.java
@@ -19,6 +19,8 @@ public class WebAppInitializer implements WebApplicationInitializer {
 		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("*.html");
+		//Add mapping for the pdf
+		dispatcher.addMapping("*.pdf");
 	}
 
 	private AnnotationConfigWebApplicationContext getContext() {
--- src/main/java/com/coding/thoughtworks/WebConfig.java
+++ src/main/java/com/coding/thoughtworks/WebConfig.java
@@ -4,20 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.coding.thoughtworks")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter{
 	
 	@Bean
 	public InternalResourceViewResolver getInternalResourceViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/jsp/");
 		resolver.setSuffix(".jsp");
-		
 		return resolver;
+	}
+	
+	/*
+	 * Added Location for the pdf s by overriding the addResourceHandlers method of WebMMvcConfigurerAdapter interface 
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addResourceHandlers(org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry)
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/pdfs/**").addResourceLocations("/WEB-INF/pdf/");
 	}
 	
 }
